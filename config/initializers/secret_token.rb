3e8d546669ccd607a63414f76fa74af770a809e6
Ramon J. Long III <ramon@evolveinc.com>
2012-08-23 14:12:29 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OpenGematria::Application.config.secret_token = '60dbc30740e90d9693426f78d36fa9a9b1c94ef10accfecb3a21b6d8aa31f2e41ee891453842649d2adb5dd5d01e20f9fbb1540a625ba2c7db0867732fdbfa9e'
