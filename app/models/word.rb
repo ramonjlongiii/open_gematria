0203df40d1b079b3a0561de9460c800558498a55
Ramon J. Long III <ramon@evolveinc.com>
2012-08-23 18:00:12 -0400

    Removes words validation

--- app/models/word.rb
+++ app/models/word.rb
@@ -1,5 +1,5 @@
 class Word < ActiveRecord::Base
   attr_accessible :gematria_desc, :gematria_value, :gematria_word, :user_id
   belongs_to :user
-  validates :gematria_desc, :gematria_value, :gematria_word
+#  validates :gematria_desc, :gematria_value, :gematria_word
 end
