e89649e557dfc696da86cee8f425960d77c5bb64
Ramon J. Long III <ramon@evolveinc.com>
2012-08-23 17:54:52 -0400

    Adds relationship: users have many words

--- app/models/word.rb
+++ app/models/word.rb
@@ -1,4 +1,5 @@
 class Word < ActiveRecord::Base
   attr_accessible :gematria_desc, :gematria_value, :gematria_word, :user_id
   validates :gematria_desc, :gematria_value, :gematria_word
+  belongs_to :user
 end
