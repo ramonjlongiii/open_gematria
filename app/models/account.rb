5b10073ad72b23c0c00e4f6a13a0329eeda29094
Ramon J. Long III <ramon@evolveinc.com>
2012-08-25 02:42:22 -0400

    Adds :dependent => :destroy to Account and HolyBook models

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,6 +1,6 @@
 class Account < ActiveRecord::Base
   attr_accessible :email, :name, :password
 
-  has_many :words
-  has_many :qbl_trees
+  has_many :words, :dependent => :destroy
+  has_many :qbl_trees, dependent => :destroy
 end
--- app/models/holy_book.rb
+++ app/models/holy_book.rb
@@ -1,5 +1,5 @@
 class HolyBook < ActiveRecord::Base
   attr_accessible :author, :edition, :isbn, :publisheddate, :publisher, :title
   
-  has_many :holy_book_verses
+  has_many :holy_book_verses, :dependent => :destroy
 end
