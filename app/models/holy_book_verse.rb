3efb96f3d841116517e832a5a2f4877d1711899b
Ramon J. Long III <ramon@evolveinc.com>
2012-08-26 00:11:31 -0400

    Adds :versecomment and :verseauthor to HolyBookVerse model

--- app/models/holy_book_verse.rb
+++ app/models/holy_book_verse.rb
@@ -1,5 +1,5 @@
 class HolyBookVerse < ActiveRecord::Base
-  attr_accessible :chapter, :holy_book_id, :versenumber, :versetext
+  attr_accessible :chapter, :holy_book_id, :versenumber, :versetext, :versecomment, :versecommentauthor
 
   belongs_to :holy_book
 end
